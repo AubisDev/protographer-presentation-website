a97f94f435558f32923afc4bd6fc5d1e4ebcb154
Aubis Sanchez <aubisst@gmail.com>
2022-02-15 21:47:07 -0400

    fixed some media queries issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,8 @@ import { useMediaQuery } from 'react-responsive';
 export const Navbar = ({  setMenuActual, setPictures, openResposiveMenu, setOpenResposiveMenu, setActualPage}) => {
 
     const [activateMenu, setActivateMenu] = useState(false)
-    const mobile = useMediaQuery({ query: '(max-width: 1024px)' })
-    const smallScreen = useMediaQuery({ query: '(max-width: 768px)' });
+    const mobile = useMediaQuery({ query: '(max-width: 1000px)' })
+    const smallScreen = useMediaQuery({ query: '(max-width: 1023px)' });
     const [toggleContact, setToggleContact] = useState(false)
 
     useEffect(() => {
@@ -45,7 +45,7 @@ export const Navbar = ({  setMenuActual, setPictures, openResposiveMenu, setOpen
         setToggleContact( !toggleContact);
     }
 
-    let menuClass = activateMenu ? `rotate-180 transition duration-500 lg:my-6 ` : 'animate__animated animate__fadeIn animate__slower'; 
+    let menuClass = activateMenu ? `rotate-180 transition duration-500 lg:my-8 ` : 'animate__animated animate__fadeIn animate__slower'; 
 
     return (
         <div className='w-4/5 h-full font-tmodel2 flex lg:flex-col m-auto'>
@@ -53,16 +53,16 @@ export const Navbar = ({  setMenuActual, setPictures, openResposiveMenu, setOpen
                {
                    !smallScreen && 
                    (
-                       <>
+                       <div className='h-full w-full flex flex-row lg:flex-col justify-evenly items-center relative top-1/4 -translate-y-1/4 md:-mt-10 lg:-mt-10 lg:-mb-40'>
                         <button 
-                            className='tracking-wide font-semibold h-full lg:h-auto animate__animated animate__fadeInDown animate__slower flex lg:flex-col flex-row  lg:-mt-10 md:mt-6 lg:mb-10'
+                            className='tracking-wide font-semibold h-full w-3/4 animate__animated animate__fadeInDown animate__slower flex flex-row md:mt-8 lg:mt-0 lg:flex-col lg:h-auto lg:-mb-10 '
                             onClick={() => setMenuActual('')}
                         >
-                            <p className='text-2xl sm:text-4xl lg:text-4xl md:-mt-1 lg:w-full lg:flex font-tmodel2 text-white  text-center items-center justify-center h-full lg:h-auto relative top-1/2 transform -translate-y-1/4 '> Manuel </p>
-                            <div className='sm:h-2/5 w-1 sm:mx-3 lg:mx-0 lg:h-1 lg:w-full -mb-4 bg-green-600 lg:my-2 lg:mt-2  sm:-mt-1 relative top-1/2 transform -translate-y-1/4  '/>
-                            <p className='text-2xl sm:text-4xl lg:text-4xl lg:mt-3 md:-mt-1 w-full flex font-tmodel2 text-white text-center justify-center h-full relative top-1/2 transform -translate-y-1/4 ' > Alegría </p>
+                            <p className='text-2xl sm:text-4xl lg:text-4xl md:mt-4 lg:w-full lg:flex font-tmodel2 text-white  text-center  lg:items-center lg:justify-center h-full lg:h-auto  '> Manuel </p>
+                            <div className='sm:h-8 w-1 sm:mx-3 lg:mx-0 lg:h-1 lg:w-full md:mt-5 bg-green-600 lg:my-2 lg:mt-2   '/>
+                            <p className='text-2xl sm:text-4xl lg:text-4xl md:mt-4 w-full flex font-tmodel2 text-white text-center lg:justify-center h-full  lg:m-0' > Alegría </p>
                             {
-                                !mobile && <p className='text-sm xl:text-base justify-center w-full flex font-tmodel2 text-white mt-4 bg-green-600 px-4 py-2 uppercase shadow-lg shadow-green-900 h-full relative top-1/2 transform -translate-y-1/4 '>Fotógrafo Profesional</p>
+                                !mobile && <p className='text-sm xl:text-base justify-center w-full flex font-tmodel2 text-white mt-4 bg-green-600 px-4 py-2 uppercase shadow-lg shadow-green-900 h-full  '>Fotógrafo Profesional</p>
                             }
                         </button>
 
@@ -70,41 +70,39 @@ export const Navbar = ({  setMenuActual, setPictures, openResposiveMenu, setOpen
                             onClick={ menuActive }
                             className = { menuClass }
                         >
-                            <i className="fas fa-bars text-3xl text-white lg:mt-6 mt-1 py-2"></i>
+                            <i className={`fas fa-bars text-3xl text-white md:mt-10 lg:${activateMenu ? 'mt-44' : 'mt-8'} py-2`}></i>
                         </button>
-                       </>
+                       </div>
                    )
                }
 
                 {
                     smallScreen &&
                     (
-                        <>
-                        <button 
-                        onClick={ menuActive }
-                        className = { menuClass }
-                        >
-                            <i className="fas fa-bars text-3xl text-white"></i>
-                        </button>
+                        <div className='h-full w-full flex flex-row justify-between items-center relative top-1/4 -translate-y-1/4'>
+                            <button 
+                                onClick={ menuActive }
+                                className = { menuClass }
+                            >
+                                <i className="fas fa-bars text-3xl text-white"></i>
+                            </button>
 
-                        <button 
-                            className='tracking-wide font-semibold h-full lg:h-auto animate__animated animate__fadeInDown animate__slower flex lg:flex-col flex-row mt-1'
-                            onClick={() => setMenuActual('')}
-                        >
-                            <p className='text-3xl sm:text-4xl lg:text-5xl  lg:w-full lg:flex -mt-2 font-tmodel2 text-white  text-center items-center justify-center h-full lg:h-auto relative top-1/2 transform -translate-y-1/4  '> Manuel </p>
-                            <div className='h-2/5 w-1 mx-3 lg:mx-0 lg:h-1 lg:w-full bg-green-600  lg:my-2 lg:mt-2 -mt-2 relative top-1/2 transform -translate-y-1/4  '/>
-                            <p className='text-3xl sm:text-4xl lg:text-5xl lg:mt-3  w-full flex font-tmodel2 text-white text-center justify-center h-full relative top-1/2 transform -translate-y-1/4 -mt-2 ' > Alegría </p>
-                            {
-                                !mobile && <p className='text-base justify-center w-full flex font-tmodel2 text-white mt-4 bg-green-600 px-4 py-2 uppercase shadow-lg shadow-green-900'>Fotógrafo Profesional</p>
-                            }
-                        </button>
-                        </>
+                            <button 
+                                className='tracking-wide font-semibold h-full w-3/4 lg:h-auto animate__animated animate__fadeInDown animate__slower flex flex-row items-center '
+                                onClick={() => setMenuActual('')}
+                            >
+                                <p className='text-3xl sm:text-4xl  l  font-tmodel2 text-white  text-center    '> Manuel </p>
+                                <div className='h-2/5 w-1 mx-3 bg-green-600     '/>
+                                <p className='text-3xl sm:text-4xl  w-full flex font-tmodel2 text-white text-center    ' > Alegría </p>
+
+                            </button>
+                        </div>
                     )
                 }
                 {
                     activateMenu && 
                     (
-                        <div className='flex flex-col border-2 border-white/40 p-4 -mt-20 rounded-lg w-90pw animate__animated animate__fadeInDown'>
+                        <div className='flex flex-col border-2 border-white/40 p-4 -mt-32 rounded-lg w-90pw animate__animated animate__fadeInDown'>
                             <div className='flex flex-row'>
                                 <button 
                                     className='w-1/2 group border-r border-b pb-1 px-3'
@@ -153,7 +151,7 @@ export const Navbar = ({  setMenuActual, setPictures, openResposiveMenu, setOpen
                     )
                 }
                 {
-                    !smallScreen && 
+                    (!smallScreen || !mobile ) && 
                     <button 
                         className='block text-xl text-white hover:text-green-600 animate__animated animate__fadeInUp animate__slower'
                         onClick={ handleToggleContact}    
